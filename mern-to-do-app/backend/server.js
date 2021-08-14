30fe3a4b6192b3ee6a49af5c8407a2d125d2e912
kagehayashi <kagehayashi@github.com>
2021-08-14 11:19:36 -0700

    Backend operational.

--- mern-to-do-app/backend/server.js
+++ mern-to-do-app/backend/server.js
@@ -7,9 +7,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
 // Connect to database
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -28,6 +25,9 @@ const tasksRouter = require('./routes/tasks');
 const usersRouter = require('./routes/users');
 app.use('/tasks/', tasksRouter);
 app.use('/users/', usersRouter);
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
